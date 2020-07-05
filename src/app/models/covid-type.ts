605cc90d565c7197a358a74b96db053a2968f41d
RAVI SHANKAR PATEL <ravishankar.patel@alight.com>
2020-07-05 22:33:47 +0530

    update tooltip

--- src/app/models/covid-type.ts
+++ src/app/models/covid-type.ts
@@ -3,11 +3,15 @@ export class CovidType
     date: string[];
     activeCases: number[];
     confirmedCases: number[];
+    activeLabelAsToolTip: any[];
+    confirmedLabelAsToolTip: any[];;
 
     constructor()
     {
         this.date = null;
         this.activeCases = null;
         this.confirmedCases = null;
+        this.activeLabelAsToolTip = null
+        this.confirmedLabelAsToolTip = null
     }
 }
